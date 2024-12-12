885ba156e0725e9705419d462e11a5b5b4848dad
Angelo Gargantini <angelo.gargantini@unibg.it>
2024-12-12 13:30:52 +0100

    vaccinazioni

---
+++ vaccinazioni/codice_di_partenza/Vaccino.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+public class Vaccino {
+	private String codice;
+	private int rischio;
+	private int anno;
+	private  Malattia[] malattia;
+	private  Vaccinazione[] vaccinazione;
+	public void Vaccino(String c, int r, int a) {
+	}
+	public void creaVaccinazione(Persona p) {
+	}
+	public void aggiungiLinkVaccinazione(Vaccinazione v) {
+	}
+	public void getPersoneVaccinate(Date d) {
+	}
+	public void aggiungiLinkMalattia(Malattia m) {
+	}
+}
---
+++ vaccinazioni/src/Persona.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Persona {
+	private String nome;
+	private String cognome;
+	private Date dataNascita;
+	public void Persona(String nome, String cognome, Date datanascita) {
+	}
+}
